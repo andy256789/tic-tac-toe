693ae1454ace3f85bec5da8f470aa81d82db6d92
andy256789 <andonovatanq3@gmail.com>
2024-02-18 13:35:22 +0200

    Made the game playable in the console

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ function Gameboard() {
     const columns = 3;
     const board = [];
 
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < columns; j++) {
-            board[i].push(Cell());
+    const resetBoard = () => {
+        board.splice(0, board.length);
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+                board[i].push(Cell());
+            }
         }
     }
 
@@ -21,7 +24,9 @@ function Gameboard() {
         board[row][col].setValue(marker);
     }
 
-    return { getBoard, printBoard, placeMarker }
+    resetBoard();
+
+    return { getBoard, printBoard, placeMarker, resetBoard }
 };
 
 function Cell() {
@@ -64,6 +69,7 @@ function GameController() {
     }
 
     const playRound = (row, col) => {
+
         console.log(`Placing ${activePlayer.name}'s marker at row: ${row + 1} column: ${col + 1}`);
         gameboard.placeMarker(row, col, activePlayer.marker);
 
@@ -71,26 +77,65 @@ function GameController() {
 
         let rowSum = 0;
         let colSum = 0;
+        let diagSum = 0;
+        let antiDiagSum = 0;
 
         boardWithCellValues[row].forEach(((col) => rowSum += col));
         boardWithCellValues.forEach((row) => colSum += row[col]);
 
-        if (rowSum === 3 || rowSum === -3 || colSum === 3 || colSum === -3) {
+        boardWithCellValues.forEach((row, Yindex) => {
+            row.forEach((cell, Xindex) => {
+                if (Yindex === Xindex) {
+                    diagSum += cell;
+                }
+            });
+        });
+
+        boardWithCellValues.forEach((row, Yindex) => {
+            row.forEach((cell, Xindex) => {
+                if ((Yindex + Xindex) === (row.length - 1)) {
+                    antiDiagSum += cell;
+                }
+            });
+        });
+
+        if (rowSum === 3 || rowSum === -3 || colSum === 3 || colSum === -3 || diagSum === 3 || diagSum === -3 || antiDiagSum === 3 || antiDiagSum === -3) {
+            gameboard.printBoard();
             console.log(`${activePlayer.name} has won the game!`);
+            gameboard.resetBoard();
+            return
         }
 
         switchActivePlayer();
         printNewRound();
     };
 
+    const promptUser = () => {
+        let row;
+        let col;
+
+        row = parseInt(prompt("Enter the row: "));
+        col = parseInt(prompt("Enter the column: "));
+
+        if (row > 3 || row < 1 || col > 3 || col < 1) {
+            console.log("Input must be either 1, 2 or 3");
+            return
+        }
+
+        if (board[row - 1][col - 1].getValue() !== 0) {
+            console.log("Invalid input");
+            return
+        }
+
+        playRound(row - 1, col - 1);
+    };
+
     printNewRound();
 
-    return { playRound };
+    return { playRound, promptUser };
 }
 
 const game = GameController();
 while (true) {
-    let row = prompt("Enter the row: ");
-    let col = prompt("Enter the column: ");
-    game.playRound(row - 1, col - 1);
+    game.promptUser();
 }
